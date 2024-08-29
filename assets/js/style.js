40d3ba0575f21153f29afeacf6dab30334f93a2d
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2024-08-29 12:49:29 +0900

    [uodate]バナーポップアップ修正

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,8 +1,33 @@
+// window.addEventListener("scroll", function () {
+//   const elm = document.querySelector(".pagetop");
+//   const scroll = window.pageYOffset;
+//   if (scroll > 500) {
+//     elm.classList.add("show");
+//   } else {
+//     elm.classList.remove("show");
+//   }
+// });
+
+
 window.addEventListener("scroll", function () {
   const elm = document.querySelector(".pagetop");
   const scroll = window.pageYOffset;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.body.offsetHeight;
+
+  // 現在の画面幅を取得
+  const windowWidth = window.innerWidth;
+
+  // スマホ画面の幅の定義（例：768px以下をスマホとみなす）
+  const isMobile = windowWidth <= 600;
+
   if (scroll > 500) {
     elm.classList.add("show");
+
+    // スマホでのみ、ページ下部に達したらボタンを消す
+    if (isMobile && scroll + windowHeight >= documentHeight - 100) {
+      elm.classList.remove("show");
+    }
   } else {
     elm.classList.remove("show");
   }
